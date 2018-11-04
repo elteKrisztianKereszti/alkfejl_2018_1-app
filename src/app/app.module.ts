c6bc30c5d3250fdf6f375cc0304a17284f26755b
Kereszti Krisztián <kereszti.krisztian@marktsoft.hu>
2018-11-04 15:44:52 +0100

    Angular first steps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule, MatIconModule, MatButtonModule, MatMenuModule } from '@angular/material';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; 
 
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IssueListComponent } from './issue-list/issue-list.component';
+import { IssueFormComponent } from './issue-form/issue-form.component';
+import { IssueDetailComponent } from './issue-detail/issue-detail.component';
+import { RoutingModule } from './routing/routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent,
+    IssueListComponent,
+    IssueFormComponent,
+    IssueDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
